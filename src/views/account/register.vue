db5784b7fc9737b6bb3d77224a6f4e0c0087c6e2
MasterOrigamii <1774136304@qq.com>
2020-12-11 14:39:05 +0800

    添加注册时实时信息合法检验及错误提示功能

--- src/views/account/register.vue
+++ src/views/account/register.vue
@@ -8,33 +8,34 @@
             <p>Please provide required information to create an account.</p>
 
             <div class="form-group">
-              <label>Username</label>
+              <label>Email</label>
               <input
-                v-validate="'required|min:3|max:16'"
-                v-model="form.model.username"
+                v-validate="'required|email'"
+                v-model="form.model.email"
                 class="form-control"
                 type="text"
-                autocomplete="username"
-                placeholder="Username"
-                name="username">
+                autocomplete="email"
+                placeholder="E-mail"
+                name="email">
               <span
-                v-show="errors.has('username')"
+                v-if="errors.has('email')"
                 class="invalid-feedback"
-                v-html="errors.first('username')" />
+                v-html="errors.first('email')" />
+
             </div>
 
             <div class="form-group">
-              <label>Name</label>
+              <label>Nickname</label>
               <input
                 v-model="form.model.name"
                 class="form-control"
                 type="text"
-                placeholder="Name"
-                name="name">
+                placeholder="Nickname"
+                name="nickname">
               <span
-                v-show="errors.has('name')"
+                v-if="errors.has('nickname')"
                 class="invalid-feedback"
-                v-html="errors.first('name')" />
+                v-html="errors.first('nickname')" />
             </div>
 
             <div class="form-group">
@@ -46,9 +47,10 @@
                 type="password"
                 autocomplete="new-password"
                 placeholder="Password"
-                name="password">
+                name="password"
+                >
               <span
-                v-show="errors.has('password')"
+                v-if="errors.has('password')"
                 class="invalid-feedback"
                 v-html="errors.first('password')" />
             </div>
@@ -56,15 +58,16 @@
             <div class="form-group">
               <label>Confirm Password</label>
               <input
-                v-validate="'confirmed'"
+                v-validate="'required|confirmed:password'"
                 v-model="form.model.confirmPassword"
                 class="form-control"
                 type="password"
                 autocomplete="new-password"
                 placeholder="Confirm Password"
-                name="confirmPassword">
+                name="confirmPassword"
+                >
               <span
-                v-show="errors.has('confirmPassword')"
+                v-if="errors.has('confirmPassword')"
                 class="invalid-feedback"
                 v-html="errors.first('confirmPassword')" />
             </div>
@@ -76,6 +79,11 @@
               value="Register">
 
             <br>
+<!--            <input v-validate="'required'" name="password" type="password" :class="{'is-danger': errors.has('password')}" placeholder="Password" ref="password">-->
+<!--            <span v-show="errors.has('password')" class="help is-danger">{{ errors.first('password') }}</span>-->
+
+<!--            <input v-validate="'required|confirmed:password'" name="password_confirmation" type="password" :class="{'is-danger': errors.has('password_confirmation')}" placeholder="Password, Again" data-vv-as="password">-->
+<!--            <span v-show="errors.has('password_confirmation')" class="help is-danger">{{ errors.first('password_confirmation') }}</span>-->
 
             <router-link
               class="btn-link"
@@ -99,7 +107,7 @@ export default {
         model: {
           password: '',
           confirmPassword: '',
-          username: '',
+          email: '',
           name: '',
         },
       },
