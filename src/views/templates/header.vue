3de916a4bbbd2ff9ce243f9c00a0bbbdcab13d6e
MasterOrigamii <1774136304@qq.com>
2020-12-14 13:42:46 +0800

    导航栏响应式显示

--- src/views/templates/header.vue
+++ src/views/templates/header.vue
@@ -1,37 +1,44 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <span class="navbar-brand">Frontend</span>
-      </li>
-      <li class="nav-item">
-        <router-link
-          class="nav-link"
-          to="/home">Home</router-link>
-      </li>
-      <li class="nav-item">
-        <router-link
-          class="nav-link"
-          to="/write">Write</router-link>
-      </li>
-      <li class="nav-item">
-        <router-link
-          class="nav-link"
-          to="/about">About</router-link>
-      </li>
-    </ul>
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <router-link
-          class="nav-link"
-          to="/account">My Account</router-link>
-      </li>
-      <li class="nav-item">
-        <span
-          class="nav-link custom-link"
-          @click="confirmLogout">Logout</span>
-      </li>
-    </ul>
+    <li class="nav-item">
+      <span class="navbar-brand">Frontend</span>
+    </li>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav">
+
+        <li class="nav-item">
+          <router-link
+            class="nav-link"
+            to="/home">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link
+            class="nav-link"
+            to="/write">Write</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link
+            class="nav-link"
+            to="/about">About</router-link>
+        </li>
+        <li class="nav-item" id="MyAccount">
+          <router-link
+            class="nav-link"
+            to="/account">My Account</router-link>
+        </li>
+        <li class="nav-item" id="Logout">
+          <span
+            class="nav-link custom-link"
+            @click="confirmLogout">Logout</span>
+        </li>
+      </ul>
+    </div>
+<!--    <ul class="navbar-nav ml-auto">-->
+
+<!--    </ul>-->
   </nav>
 </template>
 
